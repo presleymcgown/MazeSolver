451260c2cc852ef0bc11b096312542ce4d299f03
presley <presley.mcgown@icloud.com>
2022-10-03 09:05:14 -0600

    almost done, base case

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -12,30 +12,13 @@ public class MazeSolver extends GraphicsProgram {
     private Tile goalTile;
     private ArrayList<Tile> visited = new ArrayList<>();
 
+
     @Override
     public void init(){
 
       pause = Dialog.getInteger(this, "Enter the delay between steps in milliseconds.");
       maze = new Maze(22, 35, this);
       add(maze, 10 , 10);
-
-      //get a starting tile from the user
-      int r0 = Dialog.getInteger(tjos, "Enter the Row for the starting tile." + rowRange);
-
-      //get an ending tile from the user
-
-     //set the startTile based on the info you just got
-
-     //set the fill color or the starting tile to something
-
-     //set the goalTile based on the info you just got
-
-     //set the fill color or the starting tile to something
-
-     //call solve
-
-
-
 
     }
 
@@ -45,19 +28,99 @@ public class MazeSolver extends GraphicsProgram {
         String rowRange = String.format("(%d-%d):", 0, maze.getRows() - 1);
         String colRange = String.format("(%d-%d):", 0, maze.getCols() - 1);
 
+        //get a starting tile from the user
+        int r0 = Dialog.getInteger(this, "Enter the Row for the starting tile." + rowRange);
+        int c0 = Dialog.getInteger(this, "Enter the column for the starting tile." + colRange);
+
+        //get an ending tile from the user
+        int r1 = Dialog.getInteger(this, "Enter the Row for the goal tile." + rowRange);
+        int c1 = Dialog.getInteger(this, "Enter the column for the goal tile." + colRange);
+
+        //set the startTile based on the info you just got
+        startTile = maze.getTile(r0, c0);
+
+
+        //set the fill color or the starting tile to something
+        startTile.fillColor(new Color(121, 164, 117));
+
+
+        //set the goalTile based on the info you just got
+        goalTile = maze.getTile(r1, c1);
+
+        //set the fill color or the starting tile to something
+        goalTile.fillColor(new Color(169, 83, 83));
+
+
+        //call solve
+        solve(startTile, goalTile, visited);
+
     }
 
     // visited.add(), visited.contains()
     private void solve(Tile startTile, Tile goalTile, ArrayList<Tile> visited){
 
         //handle base case
+        if(visited.contains(goalTile)){
+            Dialog.showMessage("The maze was solved!");
+            return;
+        }
 
          //general cases:
+
             //add the tile you are on to the list
             //look north. If there is a possible path:
                 // set the fill color of your current tile to something
-                //move to that north tile
+                //move to that north tile (call solve and pass in tile)
                 // se fill color to white
+                //pause
+
+        if(!visited.contains(startTile)) {
+
+            visited.add(startTile);
+
+            if (!startTile.hasWall(Tile.NORTH)) {
+
+                startTile.fillColor(new Color(140, 157, 197, 171));
+                solve(startTile.getNeighbor(Tile.NORTH), goalTile, visited);
+                startTile.fillColor(Color.WHITE);
+                pause(pause);
+
+
+            }
+
+            if (!startTile.hasWall(Tile.EAST)) {
+
+                startTile.fillColor(new Color(140, 157, 197, 171));
+                solve(startTile.getNeighbor(Tile.EAST), goalTile, visited);
+                startTile.fillColor(Color.WHITE);
+                pause(pause);
+
+
+            }
+
+            if (!startTile.hasWall(Tile.SOUTH)) {
+
+                startTile.fillColor(new Color(140, 157, 197, 171));
+                solve(startTile.getNeighbor(Tile.SOUTH), goalTile, visited);
+                startTile.fillColor(Color.WHITE);
+                pause(pause);
+
+
+            }
+
+            if (!startTile.hasWall(Tile.WEST)) {
+
+                startTile.fillColor(new Color(140, 157, 197, 171));
+                solve(startTile.getNeighbor(Tile.WEST), goalTile, visited);
+                startTile.fillColor(Color.WHITE);
+                pause(pause);
+
+
+            }
+
+        }
+
+
 
     }
 
